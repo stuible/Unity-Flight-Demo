5e06d98ba4cbadaf490076cff3cf9e12662a00e6
Josh Stuible <joshstuible@gmail.com>
2020-09-27 03:49:05 -0700

    Added post-processing effects, colliders and a CharactorController

--- Assets/Scripts/MothBehaviorMouse.cs
+++ Assets/Scripts/MothBehaviorMouse.cs
@@ -7,6 +7,8 @@ public class MothBehaviorMouse : MonoBehaviour
     public float speed = 50f;
     public float lookSpeed = 90f;
     private Vector2 mousePos, screenCenter, mouseDistance;
+
+    private CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,8 @@ public class MothBehaviorMouse : MonoBehaviour
         screenCenter.y = Screen.height / 2;
 
         mousePos = new Vector2();
+
+        controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -28,15 +32,26 @@ public class MothBehaviorMouse : MonoBehaviour
         mouseDistance.x = (mousePos.x - screenCenter.x) / screenCenter.x;
         mouseDistance.y = (mousePos.y - screenCenter.y) / screenCenter.y;
 
+        //controller.Move(transform.forward * Time.deltaTime);
+
+        //controller.Move(new Vector3(0, 0, 10 * speed));
 
 
-        transform.position += transform.forward * Time.deltaTime * speed;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        controller.Move(forward * Time.deltaTime * speed);
+
+        //Debug.Log(forward * Time.deltaTime * speed);
+        //transform.position += ;
 
         speed -= transform.forward.y * Time.deltaTime * 30.0f;
 
         if (speed < 35.0f) speed = 35.0f;
 
         transform.Rotate(-mouseDistance.y * lookSpeed * Time.deltaTime, mouseDistance.x * lookSpeed * Time.deltaTime, 0f, Space.Self);
+
+
+
 
         //float terrainHeightAtMothLocation = Terrain.activeTerrain.SampleHeight(transform.position);
 
